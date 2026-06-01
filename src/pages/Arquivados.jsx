9aca3f00d7370b6495335f3446d4b0b15a4132f7
kinhowix <kinhowix@gmail.com>
2026-06-01 10:32:58 -0300

    painel_sobressaindo

--- src/pages/Arquivados.jsx
+++ src/pages/Arquivados.jsx
@@ -376,7 +376,7 @@ export default function Arquivados() {
 
       {modalEditar && (
 
-        <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center">
+        <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
 
           <div className="bg-gray-800 p-6 rounded-xl w-96">
 
@@ -459,7 +459,7 @@ export default function Arquivados() {
 
       {modalBoleto && (
 
-        <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center">
+        <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
 
           <div className="bg-gray-800 p-6 rounded-xl w-96">
 
